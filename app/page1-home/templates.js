9b5840f3cf8b5b330a28116d5b1d5f05cbd3943e
Marc McKinnis <38083453+mnm5774@users.noreply.github.com>
2018-05-24 11:59:31 -0700

    Update templates.js

--- app/page1-home/templates.js
+++ app/page1-home/templates.js
@@ -24,7 +24,7 @@ var page1Templates = {
         display: `grid`,
         border: `7px`,
         // this multiple words format does not work yet.
-        `background-image`: `url ('/photos-highres/stars1.jpg')`
+        backgroundImage: `url ('/photos-highres/stars1.jpg')`
         
         /*just for test.
         string: `
