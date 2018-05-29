8a6c00e880ad1646f4e1c84638a477e7c714d3bf
Marc McKinnis <38083453+mnm5774@users.noreply.github.com>
2018-05-29 10:27:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-/* So I was not able to dynamically render a script tag and it work seamlessly.
-I'm thinking that having that ability will be helpful later on, but for general
-purpose js I think I need to put my code in a single file, so I don't mistakenly
-collide with other code. Maybe if I wrap things in a function expression I won't
-have to worry about collision. Will have to give more thought into this if
-I end up having a ton of code here later on.
-Might be able to solve the above by having an onload function in the HTML template. */
+/* 5-29-18: Now all of my code is going to be Javascript (for the most part). So
+I will not have text-html being inserted, Javascript will create the elements
+as the application is loaded and requested.
+
+So the purpose of this file, for now, will be handling the root event listeners,
+and handling the URL requests as I learn more about that.
+*/
 
 
+
+/*
 // For sticky navbar.
 // Get the elements, using my library to create an object.
 var navbar = MM (".nav-container");
@@ -32,3 +34,4 @@ function checkNav () {
 // When the user scrolls the page, execute function to determine sticky.
 // I may need to add a debounce later to this, due to performance issues.
 window.addEventListener ('scroll', checkNav);
+*/
