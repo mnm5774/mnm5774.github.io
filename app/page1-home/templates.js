0d5875049252f53b0957c767098a5ffb7d902b41
Marc McKinnis <38083453+mnm5774@users.noreply.github.com>
2018-05-29 15:47:01 -0700

    Update templates.js

--- app/page1-home/templates.js
+++ app/page1-home/templates.js
@@ -1,13 +1,4 @@
-/* So the controller should handle the compiling of the content, and calling render.
-The template should handle the actual creating of the elements, assigning event
-listeners where needed, and I think handling and hiding each of the elements created
-as JS objects; which is possible because of my library.
-
-If we modularize each individual item here, and append them in the controller, I think
-it will make it simpler to add and use these items in other components, reducing code.
-Also, if I decide to handle external content in the controller, it will be simpler to
-incorporate it into the new internal content.
-*/
+// These are the parameters passed in the controller to create new elements.
 var page1Templates = {
   hero: function () {
     var container = {
@@ -29,7 +20,7 @@ var page1Templates = {
     
     var title = {
       type: 'h1',
-      attributes: {class: 'hero-header'},
+      attributes: {class: 'hero-title'},
       styles: {
         justifySelf: `center`,
         fontSize: `5.0vw`,
@@ -45,7 +36,7 @@ var page1Templates = {
     
     var description = {
       type: `a`,
-      attributes: {class: `hero-text`},
+      attributes: {class: `hero-description`},
       styles: {
         alignSelf: `end`,
         paddingBottom: `20px`,
@@ -54,156 +45,71 @@ var page1Templates = {
         fontSize: `1.9vw`
       },
         text: `Welcome. Here is where I showcase my portfolio, play with code and further my
-          knowledge.`};
+          knowledge.`
+    };
     
     return {container, title, description};
-        
-    // Creating a new object returns that element. This will be the parent element.
-    // need to figure out how to remove the below step.
-    /*
-    var pe = MM (parent);
-    var he = MM (header);
-    var ie = MM (intro);
-    
-    // Append the elements, all inside the parent.
-    he.append (header.text);
-    ie.append (intro.text);
-    pe.append (he, ie);*/
-    
-    /*********************************************************************************************
-    var input = {
-      type: 'input',
-      attributes: {class: 'test-input'}};
-    var button = {
-      type: 'button',
-        text: 'click here'};
-    
-    var testin = MM (input);
-    var testbt = MM (button);
-    pe.append (testin, testbt);
-    
-    // Here I will build an example function of adding new elements on demand.
-    // this probably does not belong in the template, but it is working.
-    function addText () {
-      var obj = {
-        type: 'div',
-        attributes: {class: 'unique-entry'},
-          text: testin.value};
-      var newEl = MM (obj);
-      newEl.append (obj.text);
-      pe.append (newEl);
-      console.log (pe);
-    }
-    
-    testbt.addEventListener ('click', addText);
-    
-    return pe;*/
-      
   },
 
-/*5 lines (14 sloc)  290 Bytes
- home page content. 
-#body {
-  display: grid;
-  font-family: Perpetua, TimesNewRoman;
-  margin: 0;
-  text-align: justify;
-  text-indent: 50px;
-  background-color: snow;
-  padding: 60px 30px 40px 30px;
-}
-
-  #body p {
-    font-size: 20px;
-    padding: 10px 0px 10px 0px;
-  }*/
-
-  home: function (data) {
-    var content = document.createTextNode (`
-      <link rel="stylesheet" href="app/page1-home/styles-content.css">
-
-      <div id="hello_text">
-        <h2>Hello... here's a test home page</h2>
-
-        <p>
-          Thank you for visiting my Single Page Application. I am Marc McKinnis.
-        </p>
-
-        <p>
-          This whole site is rendered using vanilla Javascript, CSS, and HTML.
-        </p>
-
-      </div>
-
-
-      <div id="home" class="page-content page1-home">
-        <header class="page-header home-header">First things first...</header>
-
-        <div class="page-intro home-intro">
-
-          <p>A little about me. I am a beginning programmer, among many other
-            things. A jack of many trades, of a sort. You can see many of my other
-            skillsets in the "Resume" section. The purpose of this site originally
-            started as an exercise for learning Javascript, and a visual aid for
-            understanding what is happening under the hood in the browser when the code
-            is parsed and executed. I soon realized the need for having a portfolio
-            available for review as well.
-          </p>
-
-          <p>So this site facilitates both of those goals. That being said, please be
-            patient if things are a bit buggy, or inaccessible. Everything you see is
-            vanilla HTML, CSS, and Javascript. I am currently not using any libraries or
-            frameworks, other than my own libraries. In the future my plan is to convert
-            this site into a Single Page Application, with the page content being dynamically
-            loaded upon request, instead of needing entirely different pages. I understand
-            this to mean a decrease in network traffic when navigating the site, and an
-            improvement in interfacing with the site. I intend to do this with vanilla
-            Javascript.
-          </p>
-
-          <p>A lot of code I have is written by me, though there are also plenty of blocks
-            I have acquired along my path of learning Javascript. In the future I will also
-            have a "Code" page, where I will store a lot of tools and projects I have acquired.
-          </p>
-
-        </div>
-
-        <div class="page-about home-about">
-            <h1 class="about-header">About Me</h1>
-
-            <div class="about-content">
-              <p>My name is Marc McKinnis. I grew up in and currently live in north-eastern
-                Arizona, in the White Mountains. I'm 23 years old.
-              </p>
-
-              <p>I have a wide range of skills, in multiple fields. I spent almost three
-                years as an I.T. Technician at Snowflake Unified School District. I also have
-                several years of experience part-time as a locksmith, working for M&G Locks.
-              </p>
-
-              <p>I currently have one job: working for Dynatrace on the Insights-Support team.
-                My duties here include data analysis on synthetic tests for web applications,
-                maintaining and creating scripts, and handling customer requests.
-              </p>
-
-              <p>I enjoy casually playing various instruments. I also am occasionally a live
-                sound technician, and enjoy going to concerts. I also like to play video
-                games (typical, I know).
-              </p>
-
-              <p>I have found that I learn new things some-what differently than most people
-                (from my point-of-view). I learn most effectively when I see the entire
-                process of things (know why I'm doing things the way I'm doing them), and see
-                how tools are used in real situations.
-              </p>
-
-            </div>
-        </div>
-
-
-        <p>Here's some filler text.</p>
+  
+  home: function () {
+    var container = {
+      type: `div`,
+      attributes: {id: `hello-text`},
+      styles: {
+        display: `grid`,
+        margin: `0`,
+        padding: `60px 30px 40px 30px`,
+        backgroundColor: `snow`,
+        fontFamily: `Perpetua, TimesNewRoman`,
+        textAlign: `justify`,
+        textIndent: `50px`
+      }
+    },
         
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis molestie
+    var header = {
+      type: `h1`,
+      text: `Hi. Welcome to my portfolio.`
+    },
+        
+    var introP1 = {
+      type: `p`,
+      text: `I am Marc McKinnis. Thank you for visiting my application. This entire site
+is rendered using vanilla Javascript. I am a beginning programmer, among many other things.
+To learn more about my skills, visit the "Resume" section.
+
+The purpose of this site originally started as an exercise for learning Javascript, and a
+visual aid for understanding what is happening under the hood in the browser when the code
+is parsed and executed. I soon realized the need for having a portfolio available for review
+as well.`
+    },
+        
+    var introP2 = {
+      type: `p`,
+      text: `So this site facilitates both of those goals. Most of the site may be a bit buggy
+for a while. Everything you see is dynamically created and rendered via vanilla Javascript. A
+lot of code I have is written by me, though there are also plenty of blocks I have acquired
+along my path of learning Javascript.`
+    },
+        
+    var about = {
+      type: `p`,
+      text: `I grew up in and currently live in north-eastern Arizona, in the White Mountains.
+I'm 23 years old. I have a wide range of skills, in multiple fields. I spent almost three years
+as an I.T. Technician at Snowflake Unified School District. I also have several years of
+experience part-time as a locksmith, working for M&G Locks.
+
+I currently have one job: working for Dynatrace on the Insights-Support team. My duties here
+include data analysis on synthetic tests for web applications, maintaining and creating scripts,
+and handling customer requests.
+
+I enjoy casually playing various instruments. I am occasionally a live sound technician, and
+enjoy going to concerts. I also like to play video games.`
+    },
+        
+    var filler = {
+      type: `p`,
+      text: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis molestie
               leo. Phasellus eu accumsan quam. Fusce sed tortor ut magna maximus tempus ac vel ex.
               Morbi in ornare mauris. Pellentesque nec lectus elit. Ut pellentesque sem leo, ut
               euismod ante condimentum et. In dignissim iaculis metus, ut volutpat nibh.
@@ -227,10 +133,9 @@ var page1Templates = {
               nisl, nec vulputate ex lectus ut libero. Etiam nec hendrerit augue, ut pulvinar
               magna. Maecenas nisi sem, ullamcorper eget luctus sit amet, rhoncus pellentesque
               odio. Vivamus euismod ipsum id volutpat commodo. Duis et volutpat velit. Fusce mauris
-              odio, euismod id cursus sit amet, luctus eleifend sem.
-            </p>
-      </div>
-    `);
-    return content;
+              odio, euismod id cursus sit amet, luctus eleifend sem.`
+    };
+    
+    return {container, header, introP1, introP2, about, filler};
   }
 };
